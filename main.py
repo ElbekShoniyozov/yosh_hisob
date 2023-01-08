b98aa9100407af4fd6bd88a5f8dddb89b3f4c85a
ElbekShoniyozov <122203165+ElbekShoniyozov@users.noreply.github.com>
2023-01-08 15:00:36 +0500

    Ikki foydalanuvchi yoshi

---
+++ main.py
@@ -0,0 +1,19 @@
+
+now=2023
+f1=input("1-Foydalanuvchi Ismingizni kiririting:  ")
+y1=int(input("Tug'ulgan yilingizni kiriting:  "))
+yosh1=now-y1
+
+f2=input("2-Foydalanuvchi Ismingizni kiriting:  ")
+y2=int(input("Tug'ulgan yilingizni kiriting:  "))
+yosh2=now-y2
+katta=yosh1-yosh2
+kichik=yosh2-yosh1
+print(f1,"siz", yosh1, 'yoshdasiz!')
+print(f2,"siz", yosh2, 'yoshdasiz!')
+if yosh1>yosh2:
+    print(f1, 'siz', f2+'dan', katta,'yosh kattasiz!')
+elif yosh1<yosh2:
+    print(f1, "siz", f2+'dan', kichik, 'yosh kichkinasiz!' )
+
+print("Dasturdan foydalanganinggiz uchun tashakkur!")
